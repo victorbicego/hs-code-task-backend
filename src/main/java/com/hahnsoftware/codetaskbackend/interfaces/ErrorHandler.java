97d14f1938666603655d39c9317e7040f7595ba5
victor.bicego <victor.bicego@gmail.com>
2023-06-14 00:32:47 +0200

    adjust error handler, adding handleExceptionInternal

--- src/main/java/com/hahnsoftware/codetaskbackend/interfaces/ErrorHandler.java
+++ src/main/java/com/hahnsoftware/codetaskbackend/interfaces/ErrorHandler.java
@@ -2,11 +2,26 @@ package com.hahnsoftware.codetaskbackend.interfaces;
 
 import com.hahnsoftware.codetaskbackend.exceptions.AttendantNotSubscribedException;
 import org.springframework.http.*;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice
 public class ErrorHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+            HttpStatusCode statusCode, WebRequest request) {
+        if (body == null) {
+            var messages = ex.getMessage() != null ? List.of(ex.getMessage()) : List.of();
+            body = Map.of("messages", messages);
+        }
+        return super.handleExceptionInternal(ex, body, headers, statusCode, request);
+    }
 
     @ExceptionHandler
     public ResponseEntity<Object> handleAttendantNotSubscribedException(AttendantNotSubscribedException ex,
