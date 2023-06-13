f9e430f0c5222b9a514e6dc0f08f5fb99d5e99d0
victor.bicego <victor.bicego@gmail.com>
2023-06-13 22:12:56 +0200

    first commit

---
+++ src/main/java/com/hahnsoftware/codetaskbackend/CodeTaskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hahnsoftware.codetaskbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeTaskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeTaskBackendApplication.class, args);
+	}
+
+}
