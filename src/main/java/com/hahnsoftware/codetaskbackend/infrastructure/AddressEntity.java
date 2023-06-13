53604edda46d4a5e4ee4c2d24b065c2e9a50ce08
victor.bicego <victor.bicego@gmail.com>
2023-06-13 22:21:54 +0200

    create address entity

---
+++ src/main/java/com/hahnsoftware/codetaskbackend/infrastructure/AddressEntity.java
@@ -0,0 +1,18 @@
+package com.hahnsoftware.codetaskbackend.infrastructure;
+
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+@Embeddable
+public class AddressEntity {
+    @NotNull
+    private String street;
+    @NotNull
+    private String immobileNumber;
+    @NotNull
+    private String city;
+    @NotNull
+    private String postalCode;
+}
